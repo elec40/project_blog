b603879983eb2fd9d507c98ce4c71970f96a943e
sangho <kimoizi@naver.com>
2022-04-25 23:36:25 +0900

    main page 작성중..

--- app.py
+++ app.py
@@ -41,21 +41,27 @@ def create_id():
 
    db.login.insert_one({'user_id': receive_user_id, 'user_password': receive_user_password})
 
-   return jsonify({'msg': "생성 완료\n로그인 페이지로 이동합니다."})
+   return jsonify({'msg': "회원가입 완료\n로그인 페이지로 이동합니다."})
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/login/login_user', methods=['POST'])
+def login_user():
    receive_user_id = request.form['user_id']
    receive_user_password = request.form['user_password']
 
-   user_id = db.login.find_one({'user_id': receive_user_id}, {'user_password': receive_user_password})
+   user_db_data = db.login.find_one({'user_id': receive_user_id})
 
-   print(user_id)
-
-   if user_id == None:
+   if user_db_data == None:
       return jsonify({'result': 1})
    else:
-      return jsonify({'result': 2})
+      if user_db_data['user_password'] != receive_user_password:
+         return jsonify({'result': 2})
+      else:
+         return jsonify({'result': 3})
+
+@app.route('/login/login_result', methods=['POST'])
+def login_user():
+   login_result = request.form['login_result']
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
